389e3dbc0da8ec78bb3bcd36b85eea157e64e5a8
vedant-max <vedanttiw168@gmail.com>
2023-06-28 19:02:26 +0530

    feat: added test dependecy

--- app/src/main/java/co/vance/nasagalleryapp/util/Extensions.kt
+++ app/src/main/java/co/vance/nasagalleryapp/util/Extensions.kt
@@ -6,7 +6,6 @@ import retrofit2.Response
 suspend fun <T> Response<T>.parse(
   onSuccess: suspend (T) -> Unit
 ) {
-
   if (isSuccessful) {
     val responseBody = body()
     if (responseBody == null) {
